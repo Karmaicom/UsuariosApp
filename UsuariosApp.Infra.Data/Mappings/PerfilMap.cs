8149fa3041538fd4e7ef7a53711a4fcf21cf287a
karmaicom <karmaicom@gmail.com>
2025-04-26 17:06:13 -0300

    feat: Mapemanto das entidades Usuario e Perfil

---
+++ UsuariosApp.Infra.Data/Mappings/PerfilMap.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UsuariosApp.Domain.Entities;
+
+namespace UsuariosApp.Infra.Data.Mappings
+{
+    public class PerfilMap : IEntityTypeConfiguration<Perfil>
+    {
+        /// <summary>
+        /// Classe para mapeamento da entidade Perfil no banco de dados
+        /// </summary>
+        /// <param name="builder"></param>
+        public void Configure(EntityTypeBuilder<Perfil> builder)
+        {
+            builder.ToTable("Perfil");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Id).HasColumnName("Id");
+            
+            builder.Property(p => p.Nome)
+                .HasColumnName("Nome")
+                .HasMaxLength(25)
+                .IsRequired();
+
+            builder.HasIndex(p => p.Nome).IsUnique();
+        }
+    }
+}
---
+++ UsuariosApp.Infra.Data/Mappings/UsuarioMap.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UsuariosApp.Domain.Entities;
+
+namespace UsuariosApp.Infra.Data.Mappings
+{
+    /// <summary>
+    /// Classe para mapeamento da entidade Usuário no banco de dados
+    /// </summary>
+    public class UsuarioMap : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.ToTable("Usuario");
+
+            builder.HasKey(u => u.Id);
+            
+            builder.Property(u => u.Id).HasColumnName("Id");
+            
+            builder.Property(u => u.Nome)
+                .HasColumnName("Nome")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(u => u.Email)
+                .HasColumnName("Email")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(u => u.Senha)
+                .HasColumnName("Senha")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(u => u.PerfilId)
+                .HasColumnName("Perfil_Id")
+                .IsRequired();
+
+            builder.HasIndex(u => u.Email).IsUnique();
+
+            builder.HasOne(u => u.Perfil) // Usuário TEM 1 Perfil
+                .WithMany(u => u.Usuarios) // Perfil TEM muitos Usuários
+                .HasForeignKey(u => u.PerfilId) // Chave estrangeira do relacionamento
+                .OnDelete(DeleteBehavior.Restrict); // Só permite exluir registros que não possuam vinculo
+        }
+    }
+}
